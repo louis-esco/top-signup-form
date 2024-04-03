bec20927043cf85c36f1566a39a61091d5980d34
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-04-03 19:39:37 +0200

    Adds more js input verifications

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const submitBtn = document.querySelector('.submit-btn button');
 const passwordInputs = document.querySelectorAll('input[type="password"]')
 const passwordAlerts = document.querySelectorAll('.password')
 
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener('click', passwordAlert);
+
+for (let passwordInput of passwordInputs) {
+    passwordInput.addEventListener('change', passwordAlert);
+}
+
+function passwordAlert() {
     if (passwordInputs[0].value !== passwordInputs[1].value) {
         for (let password of passwordAlerts) {
-            password.textContent = "Please match passwords"
+            password.textContent = "Please match passwords";
+            passwordInputs[0].style.border = "1px solid red";
+            passwordInputs[1].style.border = "1px solid red";
         }
     } else {
         for (let password of passwordAlerts) {
             password.textContent = ""
+            passwordInputs[0].style.border = "1px solid #E5E7EB";
+            passwordInputs[1].style.border = "1px solid #E5E7EB";
+
+
         }
     }
-})
+}
